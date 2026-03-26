653a9d06b94e65431c2d17f830df0172e8e245d8
Julien <julien@devanco.com>
2026-03-26 09:43:38 +0100

    refactor: convert _layout.tsx to Tailwind className

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView className="flex-1">
       <KeyboardProvider>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index" />
