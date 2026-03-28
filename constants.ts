d53d41eb0d6467c8f4bb942d7222472b58d0bdb0
Julien <julien@devanco.com>
2026-03-28 13:22:16 +0100

    espacement vert

--- constants.ts
+++ constants.ts
@@ -6,6 +6,6 @@ export const CONTAINER_WIDTH = SCREEN_WIDTH - PADDING * 2;
 export const COLUMNS = 2;
 export const GAP = 12;
 export const CARD_WIDTH = (CONTAINER_WIDTH - GAP) / COLUMNS;
-export const ROW_HEIGHT = 128 + GAP; // h-32 (128px) + gap
+export const ROW_HEIGHT = 128;
 export const LONG_PRESS_DURATION = 250;
 export const WOBBLE_DURATION = 100;
